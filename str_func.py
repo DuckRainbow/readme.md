64832105d8d5bbd89843b2cc084d603e15341549
Asya <sillestyna@gmail.com>
2024-01-16 12:27:04 +0500

    add str_func_2

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def str_upper(value: str):
+    """Make string upper"""
     return value.upper()
+
+def str_title(value: str):
+    """Make first letter in every word upper"""
+    return value.title()
